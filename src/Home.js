acbb3c45ce89da8f38960b2775c5508c783b7d47
John Whitten <johnwhitten.studio@gmail.com>
2022-06-21 20:03:03 -0700

    complete lesson #14

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,9 +13,14 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  });
+
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/> */}
     </div>
   );
